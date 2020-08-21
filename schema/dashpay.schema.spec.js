0b1a7f906fc15006cd186b360e6411322420ab19
Alex Werner <alex@werner.fr>
2020-08-21 02:16:31 +0200

    test: added not empty + not too long + is valid test for profile

--- schema/dashpay.schema.spec.js
+++ schema/dashpay.schema.spec.js
@@ -2,6 +2,7 @@ const {expect} = require('chai');
 const DashPlatformProtocol = require('@dashevo/dpp');
 const generateRandomId = require('@dashevo/dpp/lib/test/utils/generateRandomId');
 const schema = require('../schema/dashpay.schema');
+const whitepaperMasternodeText = 'Full nodes are servers running on a P2P network that allow peers to use them to receive updates about the events on the network. These nodes utilize significant amounts of traffic and other resources that incur a substantial cost. As a result, a steady decrease in the amount of these nodes has been observed for some time on the Bitcoin network and as a result, block propagation times have been upwards of 40 seconds. Many solutions have been proposed such as a new reward scheme by Microsoft Research and the Bitnodes incentive program.';
 
 describe('Dashpay Contract', () => {
   let dpp;
@@ -56,6 +57,38 @@ describe('Dashpay Contract', () => {
           expect(error.keyword).to.equal('required');
           expect(error.params.missingProperty).to.equal('displayName');
         });
+        it('should not be empty', async () => {
+          profileData.displayName = '';
+
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          const result = await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          const [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('minLength');
+          expect(error.dataPath).to.equal('.displayName');
+        });
+        it('should have less than 64 chars length', async () => {
+          profileData.displayName = 'AliceAndBobAndCarolAndDanAndEveAndFrankAndIvanAndMikeAndWalterAndWendy';
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          let result = await dpp.document.validate(profile);
+          await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          let [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('maxLength');
+          expect(error.dataPath).to.equal('.displayName');
+        });
       });
       describe('publicMessage', () => {
         it('should be defined', async () => {
@@ -73,6 +106,38 @@ describe('Dashpay Contract', () => {
           expect(error.name).to.equal('JsonSchemaError');
           expect(error.keyword).to.equal('required');
           expect(error.params.missingProperty).to.equal('publicMessage');
+        });
+        it('should not be empty', async () => {
+          profileData.publicMessage = '';
+
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          const result = await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          const [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('minLength');
+          expect(error.dataPath).to.equal('.publicMessage');
+        });
+        it('should have less than 256 chars length', async () => {
+          profileData.publicMessage = whitepaperMasternodeText;
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          let result = await dpp.document.validate(profile);
+          await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          let [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('maxLength');
+          expect(error.dataPath).to.equal('.publicMessage');
         });
       });
       describe('avatarUrl', () => {
@@ -92,7 +157,38 @@ describe('Dashpay Contract', () => {
           expect(error.keyword).to.equal('required');
           expect(error.params.missingProperty).to.equal('avatarUrl');
         });
+        it('should not be empty', async () => {
+          profileData.avatarUrl = '';
 
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          const result = await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          const [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('minLength');
+          expect(error.dataPath).to.equal('.avatarUrl');
+        });
+        it('should have less than 256 chars length', async () => {
+          profileData.avatarUrl = 'https://github.com/dashpay/dash/wiki/Whitepaper?text='+encodeURI(whitepaperMasternodeText);
+          const profile = dpp.document.create(contract, identityId, 'profile', profileData);
+
+          let result = await dpp.document.validate(profile);
+          await dpp.document.validate(profile);
+
+          expect(result.isValid()).to.be.false();
+          expect(result.errors).to.have.a.lengthOf(1);
+
+          let [error] = result.errors;
+
+          expect(error.name).to.equal('JsonSchemaError');
+          expect(error.keyword).to.equal('maxLength');
+          expect(error.dataPath).to.equal('.avatarUrl');
+        });
       });
       it('should not have additional properties', async () => {
         profileData.someOtherProperty = 42;
@@ -110,7 +206,13 @@ describe('Dashpay Contract', () => {
         expect(error.keyword).to.equal('additionalProperties');
         expect(error.params.additionalProperty).to.equal('someOtherProperty');
       });
+      it('should be valid', async () => {
+        const profile = dpp.document.create(contract, identityId, 'profile', profileData);
 
+        const result = await dpp.document.validate(profile);
+
+        expect(result.isValid()).to.be.true();
+      });
     });
     describe('Contact info', () => {
       let contactInfoData;
